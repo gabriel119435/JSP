dda856739e1c80e900e1b4b5bf97b669693834a2
Gabriel Rado <gabriel119435@gmail.com>
2017-04-25 17:21:39 -0300

    restored

--- src/controller/Serv.java
+++ src/controller/Serv.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import domain.Facade;
-import domain.Pessoa;
 
 @WebServlet("/serv")
 public class Serv extends HttpServlet {
